191ba47cddab551663e44302e58aebb2f6e807d4
ety001 <ety001@domyself.me>
2018-02-08 21:27:25 +0800

    add reply content into email and not mention yourself reply

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -27,43 +27,49 @@ class ApiController extends Controller
         $body = isset($data['body']) ? $data['body'] : null;
         $json_metadata = isset($data['json_metadata']) ? $data['json_metadata'] : null;
 
-        $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository(User::class)->findOneBy([
-            'username' => $parent_author,
-        ]);
-
-        if ($user) {
-            $settings = json_decode( $user->getSetting() , true );
-            if ( isset($settings['replies']) && $settings['replies'] == 'on') {
-                $email = $user->getEmail();
-                $reply_url = 'https://steemit.com/'.$parent_permlink.'/@'.$author.'/'.$permlink;
-                
-                $sys_email = getenv('SYS_EMAIL');
-                // send email
-                $message = (new \Swift_Message('[SteemMention] You have a NEW reply!'))
-                    ->setFrom($sys_email)
-                    ->setTo($email)
-                    ->setBody(
-                        $this->renderView(
-                            'emails/reply.html.twig',
-                            [
-                                'reply_url' => $reply_url,
-                                'author' => $author,
-                                'parent_author' => $parent_author,
-                            ]
-                        ),
-                        'text/html'
-                    );
-                $mailer->send($message);
-                $result = 'get ' . $parent_author . ' and sent an email to ' . $email;
-                $code = 1;
-            } else {
-                $result = 'get ' . $parent_author;
-                $code = 0;
-            }
+        if ($parent_author == $author) {
+            $result = 'not_mention_yourself';
+            $code = -2;
         } else {
-            $result = $parent_author . ' is not in db.';
-            $code = -1;
+            $em = $this->getDoctrine()->getManager();
+            $user = $em->getRepository(User::class)->findOneBy([
+                'username' => $parent_author,
+            ]);
+
+            if ($user) {
+                $settings = json_decode( $user->getSetting() , true );
+                if ( isset($settings['replies']) && $settings['replies'] == 'on') {
+                    $email = $user->getEmail();
+                    $reply_url = 'https://steemit.com/'.$parent_permlink.'/@'.$author.'/'.$permlink;
+                    
+                    $sys_email = getenv('SYS_EMAIL');
+                    // send email
+                    $message = (new \Swift_Message('[SteemMention] You have a NEW reply!'))
+                        ->setFrom($sys_email)
+                        ->setTo($email)
+                        ->setBody(
+                            $this->renderView(
+                                'emails/reply.html.twig',
+                                [
+                                    'reply_url' => $reply_url,
+                                    'author' => $author,
+                                    'parent_author' => $parent_author,
+                                    'body' => $body,
+                                ]
+                            ),
+                            'text/html'
+                        );
+                    $mailer->send($message);
+                    $result = 'get ' . $parent_author . ' and sent an email to ' . $email;
+                    $code = 1;
+                } else {
+                    $result = 'get ' . $parent_author;
+                    $code = 0;
+                }
+            } else {
+                $result = $parent_author . ' is not in db.';
+                $code = -1;
+            }
         }
 
         return $this->json([
