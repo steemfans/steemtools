fe447d4f44909b8321461d3e756465f0a63a66de
ety001 <ety001@domyself.me>
2018-11-03 21:30:59 +0800

    rename migrations

--- database/migrations/2018_11_03_070802_update_wxuser_table2.php
+++ database/migrations/2018_11_03_070802_update_wxuser_table2.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateWxuserTable extends Migration
+class UpdateWxuserTable2 extends Migration
 {
     /**
      * Run the migrations.
