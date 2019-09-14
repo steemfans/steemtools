1a38e7b54b57999cb1ee9a0122cc35b91a79cab9
ETY001 <ety001@domyself.me>
2019-09-15 01:12:53 +0800

    update wherein resources

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
 @section('body')
     <div class="col-xs-12 col-sm-12 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4"
         style="text-align: center;">
-        <div style="margin-top: 10%;font-size: 28px; font-weight: 900; color: #439ff6;">SteemTools</div>
-        <div><img src="/img/steemtools.jpg" /></div>
+        <div style="margin-top: 10%;font-size: 28px; font-weight: 900; color: #439ff6;">Wherein</div>
+        <div><img src="/img/wherein-bit.jpg" /></div>
         <div style="color: red;">国人轻松玩转Steemit工具集合</div>
         <div style="color: green;">赶紧扫码关注吧</div>
     </div>
